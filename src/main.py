a7dabdc1332a4e7863f7a0ace117a3552cadb82c
Oyunsky <deadcardinal293@gmail.com>
2024-07-20 15:55:28 +0700

    feat: categorized

--- src/main.py
+++ src/main.py
@@ -1,28 +1,21 @@
-from fastapi import FastAPI, Request, Depends
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
-from src.weatherapi.client import WeatherAPI, get_weather_api
+from .routers import view, api
+from .constants import BASE_DIR
 
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="src/static"), name="static")
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
-templates = Jinja2Templates(directory="src/templates")
+app.include_router(view.router)
+app.include_router(api.router)
 
-
-@app.get("/", response_class=HTMLResponse)
-async def read_home(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
-
-
-@app.get("/weather")
-async def get_weather(city: str, days: int = 1, weather_api: WeatherAPI = Depends(get_weather_api)):
-    return await weather_api.get_forecast(city, days=days)
-
-
-@app.get("/complete")
-async def get_city_name(q: str, weather_api: WeatherAPI = Depends(get_weather_api)):
-    return await weather_api.auto_complete(q)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
