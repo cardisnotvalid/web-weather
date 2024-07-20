62e0e27188110280e065a0425a4c3e6b9d93afbf
Oyunsky <deadcardinal293@gmail.com>
2024-07-20 17:34:17 +0700

    add: route to favicon

--- src/routers/view.py
+++ src/routers/view.py
@@ -1,10 +1,10 @@
 from urllib.parse import unquote
 
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 
 from .constants import COOKIE_LAST_CITY
-from ..constants import templates
+from ..constants import templates, BASE_DIR
 
 
 router = APIRouter(tags=["view"])
@@ -17,3 +17,8 @@ async def read_home(request: Request):
         last_city = unquote(last_city)
     context = {"last_city": last_city}
     return templates.TemplateResponse(request=request, name="index.html", context=context)
+
+
+@router.get("/favicon.ico", include_in_schema=True)
+async def favicon():
+    return FileResponse(BASE_DIR / "static/favicon.ico")
